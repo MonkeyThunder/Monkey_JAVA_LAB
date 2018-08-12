aa268b5431ff73df5de50327b8d1bbcf6bd9d344
Monkey Thunder <lab.monkeythunder@gmail>
2018-08-13 00:01:13 +0900

    Make Class

--- Root_Algorithm/main.cpp
+++ Root_Algorithm/main.cpp
@@ -6,9 +6,10 @@ private:
     int Map_Array_12[12][12];
 
 public:
-    void NumberToArray_12(int INT_Input,int INT_Output[12][12]);
+    void NumberToArray_12(int INT_Input);
 
 
+    int Checking_Data(int INT_X, int INT_Y);
 };
 
 class Root_Map_14{
@@ -17,9 +18,10 @@ private:
     int Map_Array_14[14][14];
 
 public:
-    void NumberToArray_14(int INT_Input,int INT_Output[14][14]);
+    void NumberToArray_14(int INT_Input);
 
 
+    int Checking_Data(int INT_X, int INT_Y);
 };
 
 
@@ -31,12 +33,17 @@ int main() {
     int Test_Num_02=140143;
     int Test_Num_03=120199;
 
+    Root_Map_14 FirstSession;
+
+    FirstSession.NumberToArray_14(Test_Num_01);
+    std::cout<< FirstSession.Checking_Data(1,1);
+
 
 
     return 0;
 }
 
-void Root_Map_12::NumberToArray_12(int INT_Input,int INT_Output[12][12])
+void Root_Map_12::NumberToArray_12(int INT_Input)
 {
     int Buff_Num_X, Buff_Num_Y, Buff_Num_Type;
 
@@ -46,11 +53,11 @@ void Root_Map_12::NumberToArray_12(int INT_Input,int INT_Output[12][12])
 
     Buff_Num_X=INT_Input/10000;
 
-    INT_Output[Buff_Num_X][Buff_Num_Y]=Buff_Num_Type;
+    Map_Array_12[Buff_Num_X][Buff_Num_Y]=Buff_Num_Type;
 
     //std::cout<< Buff_Num_X << ", " << Buff_Num_Y << " = " << Buff_Num_Type <<std::endl;
 }
-void Root_Map_14::NumberToArray_14(int INT_Input,int INT_Output[14][14])
+void Root_Map_14::NumberToArray_14(int INT_Input)
 {
     int Buff_Num_X, Buff_Num_Y, Buff_Num_Type;
 
@@ -60,7 +67,15 @@ void Root_Map_14::NumberToArray_14(int INT_Input,int INT_Output[14][14])
 
     Buff_Num_X=INT_Input/10000;
 
-    INT_Output[Buff_Num_X][Buff_Num_Y]=Buff_Num_Type;
+    Map_Array_14[Buff_Num_X][Buff_Num_Y]=Buff_Num_Type;
 
     //std::cout<< Buff_Num_X << ", " << Buff_Num_Y << " = " << Buff_Num_Type <<std::endl;
+}
+int Root_Map_12::Checking_Data(int INT_X, int INT_Y)
+{
+    return Map_Array_12[INT_X][INT_Y];
+}
+int Root_Map_14::Checking_Data(int INT_X, int INT_Y)
+{
+    return Map_Array_14[INT_X][INT_Y];
 }
