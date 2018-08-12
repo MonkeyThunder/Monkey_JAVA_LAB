40b1bef8fa0a055c8ba3142e69dd3266833f911a
Monkey Thunder <lab.monkeythunder@gmail>
2018-08-13 00:43:51 +0900

    Root Type Function

--- Root_Algorithm/main.cpp
+++ Root_Algorithm/main.cpp
@@ -4,6 +4,7 @@ class Root_Map_12{
 
 private:
     int Map_Array_12[12][12];
+    int Root_Map[16][9][2];
 
 public:
     void NumberToArray_12(int INT_Input);
@@ -16,15 +17,17 @@ class Root_Map_14{
 
 private:
     int Map_Array_14[14][14];
+    int Root_Map[16][13][2];
 
 public:
     void NumberToArray_14(int INT_Input);
+    void LinkToCenter(int INT_Input);
 
 
     int Checking_Data(int INT_X, int INT_Y);
 };
 
-
+void Root_Type_xy(int ForE, int Type, int *INT_X, int *INT_Y);
 
 int main() {
 
@@ -71,6 +74,120 @@ void Root_Map_14::NumberToArray_14(int INT_Input)
 
     //std::cout<< Buff_Num_X << ", " << Buff_Num_Y << " = " << Buff_Num_Type <<std::endl;
 }
+void Root_Map_14::LinkToCenter(int INT_Input)
+{
+    int Buff_Num_X, Buff_Num_Y, Buff_Num_Type;
+
+    Buff_Num_Type=INT_Input%100;
+
+    Buff_Num_Y=(INT_Input%10000-Buff_Num_Type)/100;
+
+    Buff_Num_X=INT_Input/10000;
+
+    Map_Array_14[Buff_Num_X][Buff_Num_Y]=Buff_Num_Type;
+
+
+
+}
+
+void Root_Type_xy(int ForE, int Type,int *INT_X, int *INT_Y)
+{
+    int Buff_Num01;
+
+    Buff_Num01=Type%10;
+
+    if(Type>70)
+    {
+        switch(Buff_Num01)
+        {
+            case 3:
+                   *INT_Y=*INT_Y-1;
+                break;
+            case 4:
+                   *INT_X=*INT_X+1;
+                break;
+            case 5:
+                   *INT_Y=*INT_Y+1;
+                break;
+            case 6:
+                   *INT_X=*INT_X-1;
+                break;
+            default:
+                break;
+        }
+    }
+    else
+    {
+        switch(Buff_Num01)
+        {
+            case 3:
+                if(ForE==0)
+                {
+                    *INT_Y=*INT_Y-1;
+                }
+                else
+                {
+                    *INT_Y=*INT_Y+1;
+                }
+                break;
+            case 4:
+                if(ForE==0)
+                {
+                    *INT_X=*INT_X-1;
+                }
+                else
+                {
+                    *INT_X=*INT_X+1;
+                }
+                break;
+            case 5:
+                if(ForE==0)
+                {
+                    *INT_Y=*INT_Y-1;
+                }
+                else
+                {
+                    *INT_X=*INT_X+1;
+                }
+                break;
+            case 6:
+                if(ForE==0)
+                {
+                    *INT_X=*INT_X+1;
+                }
+                else
+                {
+                    *INT_Y=*INT_Y+1;
+                }
+                break;
+            case 7:
+                if(ForE==0)
+                {
+                    *INT_X=*INT_X-1;
+                }
+                else
+                {
+                    *INT_Y=*INT_Y+1;
+                }
+                break;
+            case 8:
+                if(ForE==0)
+                {
+                    *INT_Y=*INT_Y-1;
+                }
+                else
+                {
+                    *INT_X=*INT_X-1;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
+
 int Root_Map_12::Checking_Data(int INT_X, int INT_Y)
 {
     return Map_Array_12[INT_X][INT_Y];
