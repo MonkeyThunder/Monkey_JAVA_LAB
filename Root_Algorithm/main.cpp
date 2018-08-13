478ed7305d97146999f00b93836b0dcbb0f9da41
Monkey Thunder <lab.monkeythunder@gmail>
2018-08-13 16:57:13 +0900

    Distance With Center

--- Root_Algorithm/main.cpp
+++ Root_Algorithm/main.cpp
@@ -51,8 +51,7 @@ int main() {
     return 0;
 }
 
-void Root_Map_12::NumberToArray_12(int INT_Input)
-{
+void Root_Map_12::NumberToArray_12(int INT_Input) {
     int Buff_Num_X, Buff_Num_Y, Buff_Num_Type;
 
     Buff_Num_Type=INT_Input%100;
@@ -66,8 +65,7 @@ void Root_Map_12::NumberToArray_12(int INT_Input)
     //std::cout<< Buff_Num_X << ", " << Buff_Num_Y << " = " << Buff_Num_Type <<std::endl;
 }
 
-void Root_Map_14::NumberToArray_14(int INT_Input)
-{
+void Root_Map_14::NumberToArray_14(int INT_Input) {
     int Buff_Num_X, Buff_Num_Y, Buff_Num_Type;
 
     Buff_Num_Type=INT_Input%100;
@@ -80,8 +78,7 @@ void Root_Map_14::NumberToArray_14(int INT_Input)
 
     //std::cout<< Buff_Num_X << ", " << Buff_Num_Y << " = " << Buff_Num_Type <<std::endl;
 }
-void Root_Map_14::Center_Location(int Center_INDEX, int *INT_X, int *INT_Y)
-{
+void Root_Map_14::Center_Location(int Center_INDEX, int *INT_X, int *INT_Y) {
     switch (Center_INDEX)
     {
         case 1:
@@ -152,32 +149,16 @@ void Root_Map_14::Center_Location(int Center_INDEX, int *INT_X, int *INT_Y)
             break;
     }
 }
-int Root_Map_14::DistanceToCenter(int Center_INDEX, int INT_X, int INT_Y)
-{
-    if(INT_X<9&&INT_Y<9)
-    {
+int Root_Map_14::DistanceToCenter(int Center_INDEX, int INT_X, int INT_Y) {
+    int Buff_Num_X, Buff_Num_Y;
 
-    }
-    else if(INT_X>4&&INT_Y<9)
-    {
+    Center_Location(Center_INDEX, Buff_Num_X, Buff_Num_Y);
 
-    }
-    else if(INT_X>4&&INT_Y>4)
-    {
-
-    }
-    else if(INT_X<9&&INT_Y>4)
-    {
-
-    }
-    else
-    {
-        return 0;
-    }
+    return abs(INT_X+INT_Y-Buff_Num_X-Buff_Num_Y);
 }
 
-void Root_Map_14::LinkToCenter(int INT_Input)
-{
+
+void Root_Map_14::LinkToCenter(int INT_Input) {
     int Buff_Num_X, Buff_Num_Y, Buff_Num_Type;
 
     Buff_Num_Type=INT_Input%100;
@@ -188,19 +169,15 @@ void Root_Map_14::LinkToCenter(int INT_Input)
 
     Map_Array_14[Buff_Num_X-1][Buff_Num_Y-1]=Buff_Num_Type;
 
-    //std::cout<<Buff_Num_X<<", "<<Buff_Num_Y<<std::endl;
-
     Root_Type_xy(0,Buff_Num_Type,&Buff_Num_X,&Buff_Num_Y);
 
-    //std::cout<<Buff_Num_X<<", "<<Buff_Num_Y<<std::endl;
 
 
 
 }
 
 
-void Root_Type_xy(int ForE, int Type,int *INT_X, int *INT_Y)
-{
+void Root_Type_xy(int ForE, int Type,int *INT_X, int *INT_Y) {
     int Buff_Num01;
 
     Buff_Num01=Type%10;
@@ -297,11 +274,9 @@ void Root_Type_xy(int ForE, int Type,int *INT_X, int *INT_Y)
 
 }
 
-int Root_Map_12::Checking_Data(int INT_X, int INT_Y)
-{
+int Root_Map_12::Checking_Data(int INT_X, int INT_Y) {
     return Map_Array_12[INT_X][INT_Y];
 }
-int Root_Map_14::Checking_Data(int INT_X, int INT_Y)
-{
+int Root_Map_14::Checking_Data(int INT_X, int INT_Y) {
     return Map_Array_14[INT_X][INT_Y];
 }
